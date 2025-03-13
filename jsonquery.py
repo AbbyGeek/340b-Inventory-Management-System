f04667732efe7f3ea724c727e999b4f5d275ea3d
Abby <abbyrosewest@gmail.com>
2025-03-13 16:28:41 -0400

    tweaked the data returned in med_dict, organized printing

--- jsonquery.py
+++ jsonquery.py
@@ -4,10 +4,12 @@ import os
 loadfile = open('drug-ndc-0001-of-0001.json')
 dataset = json.load(loadfile)
 
-def MedFormatting(item):
+def MedFormatting(item, ndc_input):
     generic_name = item.get('generic_name')
-    labeler_name = item.get('labeler_name')
     brand_name = item.get('brand_name')
+    dosage_form = item.get('dosage_form')
+    pharm_class = item.get('pharm_class')
+    route = item.get('route')
     ingredient_list = []
     for ingredient in item.get('active_ingredients'):
         ingredient_set = {ingredient['name']},{ingredient['strength']}
@@ -17,7 +19,7 @@ def MedFormatting(item):
         package_set = {package['package_ndc']},{package['description']},{package['marketing_start_date']},{package['sample']}
         package_list.append(package_set)
     pakcage_info = PackageLookup(package_list)
-    med_dict = dict(generic_name = generic_name, manufacturer = labeler_name, brand_name = brand_name, package_info = pakcage_info, ingredients = ingredient_list)
+    med_dict = dict(ndc_code = ndc_input, generic_name = generic_name, brand_name = brand_name, package_info = pakcage_info, dosage_form = dosage_form, ingredients = ingredient_list, route = route, pharm_class = pharm_class)              
     return med_dict
 
 def DatabaseLookup(ndc_number):
@@ -42,7 +44,6 @@ def PackageLookup(packages):
         package_ndc = package[0].pop()
         if ndc_input == package_ndc.replace("-",""):
             return package[1].pop()
-        
 
 os.system('clear')
 user_input = ''
@@ -52,29 +53,36 @@ while user_input != 'quit':
         ndc_input = UpcToNdc(user_input)
         search_item = DatabaseLookup(ndc_input)
         if search_item is not None:
-            med_dict = MedFormatting(search_item)
+            med_dict = MedFormatting(search_item, ndc_input)
+            print("NDC Code: " + med_dict["ndc_code"])
             print("Generic Name: " + med_dict['generic_name'])
-            print("Manufacturer: " + med_dict['manufacturer'])
             print("Brand Name: " + med_dict['brand_name'])
             print("Package Info: " + med_dict['package_info'])
+            print("Dosage Form: " + med_dict['dosage_form'])
+            print ("Drug Class: ")
+            for med_class in med_dict['pharm_class']:
+                print("    - " + med_class)
+            print("Route: ")
+            for route in med_dict['route']:
+                print("    - " + route)
             print("Active Ingredients:")
             for ingredient in med_dict['ingredients']:
                 ingredient_name = ingredient[0].pop()
                 ingredient_dosage = ingredient[1].pop()
-                print(ingredient_name + ": " + ingredient_dosage)
+                print("    - " + ingredient_name + ": " + ingredient_dosage)
         else:
             print("No item found")
     print('End of Operation')
 
-    #test codes
-    # Lisinopril 5mg - 
-    # 368180513018
-    # 6810-513-01  (9 digits)
-    #returns Generic name, Manufacturer, brand name, package info (100 tablet in 1 bottle), active ingredients
+"""
+    test codes
+    Lisinopril 5mg - 
+    368180513018
+    6810-513-01  (9 digits)
+    returns Generic name, Manufacturer, brand name, package info (100 tablet in 1 bottle), active ingredients
 
-    # Hydrocortizone 2.5% - 
-    # 351672300326
-    # 51672-3003-2 (10 digits)
-    #returns: Generic name, manufacturer, brand name, package info (1 tube in 1 carton, 28.35g/tube), active ingredients
-
-    
+    Hydrocortizone 2.5% - 
+    351672300326
+    51672-3003-2 (10 digits)
+    returns: Generic name, manufacturer, brand name, package info (1 tube in 1 carton, 28.35g/tube), active ingredients
+"""
