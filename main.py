e9a188411ce87d75a2e0c0e72cf32f414a07650a
Abby <abbyrosewest@gmail.com>
2025-03-24 15:54:21 -0400

    removed extra code, updated to-do

--- main.py
+++ main.py
@@ -1,28 +1,23 @@
 import os
 from jsonquery import UpcToNdc, DatabaseLookup, MedFormatting
 from inventorydb import CreateDb, AddMed
+from db_display import FetchDataAsync, GUISetup
 """
 TODO: 
--Highlight medications when below certain quantity to alert to low stock
--Add/Subtract from inventory switch
 -display most recent added/removed med under table
-
+-Metformin 500mg and Metformin 1000mg are the only two medications so far that have been causing errors when scanning
+-table should fill window
+-issues with buspirone 5mg
+-text box should always stay selected
+- Tell Yashita Redy to go fuck herself and that I've never been happier since getting kicked from her team.
+    I'm not a failure. I'm not a waste. I'm not the best programmer in the world, but I can figure this out.
 """
-
+ 
 def Main():
     os.system('clear')
-    from db_display import GUISetup
     CreateDb()
     GUISetup()
-    user_input = ''
-    while user_input != 'quit':
-        user_input = input('Product NDC or \'quit\' to exit: ')
-        if user_input != 'quit':
-            ndc_input = UpcToNdc(user_input)
-            search_item = DatabaseLookup(ndc_input)
-            if search_item is not None:
-                med_dict = MedFormatting(search_item, ndc_input)
-                AddMed(med_dict)
+    FetchDataAsync()
 
 Main()
 
@@ -33,4 +28,10 @@ Main()
 
     Hydrocortizone 2.5% - 
     351672300326
+
+    Metformin 500mg
+    365862008015
+
+    Metformin 100mg
+    370010065017
 """
